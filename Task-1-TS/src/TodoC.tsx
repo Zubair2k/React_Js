43a9d38dc19ec3a56c9e94554e80cf6e1776871f
Zubair2k <96760145+Zubair2k@users.noreply.github.com>
2022-06-07 19:21:51 +0530

    Update TodoC.tsx

--- Task-1-TS/src/TodoC.tsx
+++ Task-1-TS/src/TodoC.tsx
@@ -1,29 +1,49 @@
-import React,{FunctionComponent as FC} from 'react';
-import TODOI from "./TodoInterface";
-import Form from './Form';
+import React,{FunctionComponent as FC,useState} from 'react';
+
 import './App.css';
 
 interface Props{
+    changeDelete:any;
     cont : any;
 }
 
-const TodoC:FC<Props>=({cont})=> {
-    const handleText=(num:any)=>{
-        console.log(num)
-        if(num.style["text-decoration"]==="line-through")
-        {
-            num.style["text-decoration"]=""
-        }
-        else{
-            num.style["text-decoration"]="line-through"
-        }
+const TodoC:FC<Props>=({cont,changeDelete})=> {
+
+    const [updatebtn,setUpdatebtn]=useState<boolean>(false);
+    const [strike,setStrike]=useState<boolean>(false)
+    const [visible,setVisible]=useState<boolean>(false)
+    const [editbtn,setEditbtn]=useState<boolean>(true)
+    
+
+    const handleCheck=(e:any)=>{
+        setStrike(ps => !ps)
+        setUpdatebtn(false);
+        setEditbtn(!e.target.checked)
     }
+
+    const handleDelete=(numdel:any)=>{
+       changeDelete(numdel.id);
+    }
+
+    const handleEdit=()=>{
+        setEditbtn(false)
+       setUpdatebtn(true);
+    }
+
+    const handleUpdate=()=>{
+        setUpdatebtn(false);
+        setEditbtn(true)
+    }
+
     return (
         <div className="TodoC">
-            <h3 id={cont.id} onClick={(e)=>{handleText(e.target)}}>{cont.text}</h3>
-            <button>X</button>
-            <button>Edit</button>
-        </div>
+            <input type="checkbox" id={cont.id} onClick={handleCheck} name={cont.text} />
+            <h3 id={cont.id} contentEditable={updatebtn} className={strike ? "check":""}>{cont.text}</h3>
+            {editbtn  && <button id={cont.id} onClick={(e)=>handleDelete(e.target)}>X</button>}
+            {editbtn &&<button id={cont.id} onClick={handleEdit}> Edit</button> } 
+            {updatebtn &&<button id={cont.id} onClick={handleUpdate}>Update</button>}
+            
+    </div>
     )
 }
 
