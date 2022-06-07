2493e30a5fcddcf57d9da1ad881282f1890ae927
Zubair2k <96760145+Zubair2k@users.noreply.github.com>
2022-06-07 19:20:36 +0530

    Update Form.tsx

--- Task-1-TS/src/Form.tsx
+++ Task-1-TS/src/Form.tsx
@@ -1,13 +1,24 @@
-import React,{FormEvent, FunctionComponent as FC, useEffect, useState} from 'react';
+import React,{FormEvent, FunctionComponent as FC, useState} from 'react';
 import TODOI from "./TodoInterface";
 import TodoC from './TodoC';
 import './App.css';
 
 const Form:FC =()=> {
     let id=0;
+    let s:any;
+    const [updatedvalue,setUpdatedvalue]=useState<TODOI[]>([])
+    const [edit, setEdit] = useState<boolean>(true);
     const [todo,setTodo]=useState<string>("");
     const [toggle,setToggle]=useState<boolean>(true);
     const [todos,setTodos]=useState<TODOI[]>([]);
+
+
+    const changeDelete=(delid:any)=>{
+        console.log(delid)
+        let remainingvalue=todos.filter((deldata)=>deldata.id!=delid)
+        console.log(remainingvalue)
+        setTodos(remainingvalue);
+    }
 
     const handleChange=(e:any)=>{
        setTodo(e.target.value)
@@ -15,25 +26,24 @@ const Form:FC =()=> {
 
     const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
-        if(!todo)
-        {
-            return alert("Please fill the text field")
+            if(!todo)
+            {
+                return alert("Please fill the text field")
+            }
+            else
+            {
+                let arr=todos;
+                id=arr.length
+                arr.push({
+                    id:id,
+                    text:todo
+                })
+                setTodos(arr)        
+            }      
+            setTodo("")   
+            setToggle(!toggle);        
         }
-        else{
-            let arr=todos;
-            id=arr.length
-            arr.push({
-                id:id,
-                text:todo
-            })
-            console.log(arr)
-            setTodos(arr)
-            
-        }      
-        setToggle(!toggle);
         
-    }
-    
     return (
         <div className="Form">
             <h1>TODO APP</h1>
@@ -45,9 +55,8 @@ const Form:FC =()=> {
                 <h1>THINGS</h1>
                 <hr/>
                 {todos.map((cont) => (
-                    <TodoC cont={cont}/>
-              ))}
-     
+                    <TodoC cont={cont} changeDelete={changeDelete}/>
+                ))}    
             </div>
         </div>
     )
